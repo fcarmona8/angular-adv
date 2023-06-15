0cef29ea23a5d1b1ccb462a7a269acaaf3eee74c
Ferran Carmona <ferrancarmona2000@gmail.com>
2023-06-15 15:22:09 +0200

    fin seccion 3 - diseño

---
+++ src/app/pages/progress/progress.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ptogress',
+  templateUrl: './progress.component.html',
+  styles: [
+  ]
+})
+export class ProgressComponent {
+
+}
