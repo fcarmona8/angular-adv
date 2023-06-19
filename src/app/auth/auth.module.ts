084962213e2e9889eb3c5a48846b85cdcc34b7f1
Ferran Carmona <ferrancarmona2000@gmail.com>
2023-06-19 12:04:29 +0200

    Fin seccion 7

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -14,7 +16,8 @@ import { RegisterComponent } from './register/register.component';
       RegisterComponent],
   imports: [
     CommonModule,
-    
+    RouterModule,
+    FormsModule
   ]
 })
 export class AuthModule { }
